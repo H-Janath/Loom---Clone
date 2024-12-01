c55844e45869d105a71ccb345c6ff0d9b868d57b
H-Janath <janathhma@gmail.com>
2024-12-01 18:32:48 +0530

    Inviting users into the workspace

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -10,32 +10,50 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { userQueryData } from "@/hooks/userQueryData";
 import { getWorksPaces } from "@/actions/workspace";
-import { WorkspaceProps } from "@/types/index.type";
+import { NotificationProps, WorkspaceProps } from "@/types/index.type";
 import Modal from "../modal";
-import { PlusCircle } from "lucide-react";
+import { Menu, PlusCircle } from "lucide-react";
 import Search from "../search";
+import { MENU_ITEMS } from "@/constant";
+import SidebarItems from "./sidebar-items";
+import { getNotifications } from "@/actions/user";
+import { it } from "node:test";
+import WorkspacePlaceholder from "./WorkspacePlaceholder";
+import GlobalCard from "../global-card";
+import { Button } from "@/components/ui/button";
+import Loader from "../loader";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import InfoBar from "../info-bar";
 
 type Props = {
   activeWorkspaceId: string;
 };
 
-
 function Sidebar({ activeWorkspaceId }: Props) {
   const route = useRouter();
+  const pathName = usePathname();
+
   const { data, isFetched } = userQueryData(["user-workspace"], getWorksPaces);
+
+  const menuItems = MENU_ITEMS(activeWorkspaceId);
+  const { data: notifications } = userQueryData(
+    ["user-notifications"],
+    getNotifications
+  );
   const { data: workspace } = data as WorkspaceProps;
+  const { data: count } = notifications as NotificationProps;
 
   const onChangeActiveWorkspace = (value: string) => {
     route.push(`/dasboard/${value}`);
   };
   const currentWorkspace = workspace.workspace.find(
-    (s)=>s.id===activeWorkspaceId
-  )
+    (s) => s.id === activeWorkspaceId
+  );
 
-  return (
+  const SidebarSection = (
     <div className="bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden">
       <div className="bg-[#111111] p-4 flex gap-2 justify-center items-center mb-4 absolute top-0 left-0 right-0 ">
         <Image src={"/opal-logo.svg"} height={40} width={40} alt="logo" />
@@ -72,25 +90,131 @@ function Sidebar({ activeWorkspaceId }: Props) {
           </SelectGroup>
         </SelectContent>
       </Select>
-       {currentWorkspace?.type==="PUBLIC"&&<Modal
-            trigger={
-              <span className="text-sm cursor-pointer flex items-center justify-center bg-neutral-800/90  hover:bg-neutral-800/60 w-full rounded-sm p-[5px] gap-2">
-                <PlusCircle
-                  size={15}
-                  className="text-neutral-800/90 fill-neutral-500"
-                />
-                <span className="text-neutral-400 font-semibold text-xs">
-                  Invite To Workspace
-                </span>
+      {currentWorkspace?.type === "PUBLIC" && (
+        <Modal
+          trigger={
+            <span className="text-sm cursor-pointer flex items-center justify-center bg-neutral-800/90  hover:bg-neutral-800/60 w-full rounded-sm p-[5px] gap-2">
+              <PlusCircle
+                size={15}
+                className="text-neutral-800/90 fill-neutral-500"
+              />
+              <span className="text-neutral-400 font-semibold text-xs">
+                Invite To Workspace
               </span>
-            }
-            title="Invite To Workspace"
-            description="Invite other users to your workspace"
-          >
-            <Search workspaceId={activeWorkspaceId}/>
-          </Modal>}
+            </span>
+          }
+          title="Invite To Workspace"
+          description="Invite other users to your workspace"
+        >
+          <Search workspaceId={activeWorkspaceId} />
+        </Modal>
+      )}
+      <p className="w-full text-[#9D9D9D4] font-bold mt-4">Menu</p>
+      <nav className="w-full">
+        <ul>
+          {menuItems.map((item) => (
+            <SidebarItems
+              href={item.href}
+              icon={item.icon}
+              selected={pathName === item.href}
+              title={item.title}
+              key={item.title}
+              notifications={
+                (item.title === "notifications" &&
+                  count._count &&
+                  count._count.notification) ||
+                0
+              }
+            />
+          ))}
+        </ul>
+      </nav>
+      <Separator className="w-4/5" />
+      <p className="w-full text-[#9D9D9D] font-bold mt-4">Workspaces</p>
+
+      {workspace.workspace.length === 1 && workspace.members.length === 0 && (
+        <div className="w-full mt-[-10px]">
+          <p className="text-[#3c3c3c] font-medium text-sm">
+            {workspace.subscription?.plan === "FREE"
+              ? "Upgrade to create Workspace"
+              : "No workspaces"}
+          </p>
+        </div>
+      )}
+      <nav className="w-full">
+        <ul className="h-[150px] overflow-auto overflow-x-hidden fade-layer">
+          {workspace.workspace.length > 0 &&
+            workspace.workspace.map(
+              (item) =>
+                item.type !== "PERSONAL" && (
+                  <SidebarItems
+                    href={`/dasboard/${item.id}`}
+                    selected={pathName === `/dashboard/${item.id}`}
+                    title={item.name}
+                    notifications={0}
+                    key={item.name}
+                    icon={
+                      <WorkspacePlaceholder>
+                        {item.name.charAt(0)}
+                      </WorkspacePlaceholder>
+                    }
+                  />
+                )
+            )}
+          {workspace.members.length > 0 &&
+            workspace.members.map((item) => (
+              <SidebarItems
+                href={`/dasboard/${item.WorkSpace.id}`}
+                selected={pathName === `/dashboard/${item.WorkSpace.id}`}
+                title={item.WorkSpace.name}
+                notifications={0}
+                key={item.WorkSpace.name}
+                icon={
+                  <WorkspacePlaceholder>
+                    {item.WorkSpace.name.charAt(0)}
+                  </WorkspacePlaceholder>
+                }
+              />
+            ))}
+        </ul>
+      </nav>
+      <Separator className="w-4/5"/>
+      {workspace.subscription?.plan==='FREE' &&(
+        <GlobalCard
+         title="Upgrade to Pro"
+         description="Unloack AI features like transition, AI summary, and more"
+         footer={
+          <Button className="text-sm w-full">
+            <Loader>
+              Update
+            </Loader>
+          </Button>
+         }
+         />
+        )}
     </div>
   );
+  return <div className="full ">
+    {<InfoBar/>}
+    {}
+    <div className="md:hidden fixed my-4">
+      <Sheet>
+        <SheetTrigger
+          asChild
+          className="ml-2" 
+        >
+          <Button variant={"ghost"} className="mt-[2px]">
+            <Menu/>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side={'left'}
+        className="p-0 w-fit h-full">
+          {SidebarSection}
+        </SheetContent>
+      </Sheet>
+    </div>
+    <div className="md:block hidden h-full">{SidebarSection}</div>
+  </div>
 }
 
 export default Sidebar;

599be869725908f3c43694e834f62f0f0435675d
H-Janath <janathhma@gmail.com>
2024-12-14 06:37:03 +0530

    Create My library page

--- src/components/global/global-header/index.tsx
+++ src/components/global/global-header/index.tsx
@@ -1,27 +1,26 @@
-'use client'
-import { WorkSpace } from '@prisma/client'
-import { usePathname } from 'next/navigation'
-import React from 'react'
+"use client";
+import { WorkSpace } from "@prisma/client";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 type Props = {
-    workspace: WorkSpace
-}
+  workspace: WorkSpace;
+};
 
-const GlobalHeader = ({workspace}: Props) => {
-    const pathName = usePathname().split(`/dashboard/${workspace.id}`)[1];
+const GlobalHeader = ({ workspace }: Props) => {
+  const pathName = usePathname().split(`/dashboard/${workspace.id}`)[1];
   return (
-    <article className='flex flex-col gap-2'>
-      <span className='text-[#7070] text-xs'>
+    <article className="flex flex-col gap-2">
+      <span className="text-[#707070] text-xs">
         {workspace.type.toLocaleUpperCase()}
       </span>
-      <h1 className='text-4xl font-bold'>
-        {
-          pathName && !pathName.includes('folder') ? pathName.charAt(0).toUpperCase()+pathName.slice(1).toLowerCase()
-          :'My Library'
-        }
+      <h1 className="text-4xl font-bold">
+        {pathName && !pathName.includes("folder")
+          ? pathName.charAt(0).toUpperCase() + pathName.slice(1).toLowerCase()
+          : "My Library"}
       </h1>
     </article>
-  )
-}
+  );
+};
 
-export default GlobalHeader
+export default GlobalHeader;
